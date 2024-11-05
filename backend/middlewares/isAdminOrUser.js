8719a44ecc7a81ce3f305681c6b49d969dd5c43e
notnrj <nrjrpl007@gmail.com>
2024-11-05 23:45:48 +0530

    Add isAdminOrUser middleware

---
+++ backend/middlewares/isAdminOrUser.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const isAdminOrUser = async (req, res, next) => {
+    const authorizationHeader = req.headers.authorization;
+
+    if (!authorizationHeader) {
+        res.status(400).json({ error: "Token is missing.", success: false });
+        return;
+    }
+    else {
+        try {
+            const token = authorizationHeader.split(" ")[1];
+            const data = jwt.verify(token, process.env.SECRET);
+            if (data.designation.toLowerCase() === "admin") {
+                req.name = data.name
+                req.designation = data.designation
+                next()
+            } else if (data.designation.toLowerCase() === "user") {
+                req.name = data.name
+                req.designation = data.designation
+                next()
+            } else {
+                res.status(401).json({ message: "Unauthorized", success: false });
+            }
+        } catch (error) {
+            res.status(401).json({ message: "An internal server error occurred while authorizing the admin/user: " + error.message, success: false });
+        }
+    }
+};
+
+module.exports = isAdminOrUser;
