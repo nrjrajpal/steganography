5c09c1388de925c098cf2b49e5203e09edc752f8
Adi-Samudra <adityasamudra369@gmail.com>
2024-11-06 00:58:03 +0530

    Add updateAdmin Api

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -46,4 +46,60 @@ router.post("/addAdmin", async (req, res) => {
     }
 })
 
+router.post("/updateAdmin", async (req, res) => {
+    try {
+        const { name, adminID, password,email } = req.body;
+        if (adminID == null || name == null || email == null) {
+            res.status(400).json({ message: "The request body doesn't contain all the fields that are required to update admin", success: false });
+            return
+        }
+
+        const existingAdmin = await Admin.findOne({ adminID });
+        let updatedFields;
+        if (existingAdmin) {
+            const adminE = await Admin.findOne({ email })
+            if (adminE && email!== adminE.email) {
+                res.status(409).json({ message: "An admin account with this email already exists", success: false });
+                return
+            }
+            const userE = await User.findOne({ email })
+            if (userE) {
+                res.status(409).json({ message: "A user account with this email already exists", success: false });
+                return
+            }
+
+            if(password == null){
+                updatedFields = { name, password:existingAdmin.password, email, joiningDate:existingAdmin.joiningDate };
+            }
+            else{
+                const salt = await bcrypt.genSalt(10);
+                const hash = await bcrypt.hash(password, salt);
+                updatedFields = { name, password:hash, email, joiningDate:existingAdmin.joiningDate };
+            }
+            const updatedDocument = await Admin.findOneAndUpdate(
+                { adminID },
+                updatedFields,
+                { new: true }
+            );
+
+            if (updatedDocument) {
+                res.status(200).json({ message:"Update Admin details", admin:updatedDocument,success: true });
+            } else {
+                res.status(500).json({ message: "Failed to update admin.", success: false });
+            }
+        } else {
+            res.status(404).json({
+                error: `Admin with adminID ${adminID} does not exist.`,
+                success: false
+            });
+        }
+    } catch (error) {
+        console.error("An error occurred while updating the admin:", error);
+        res.status(500).json({
+            error: "An internal server error occurred while updating the admin.",
+            success: false
+        });
+    }
+});
+
 module.exports = router
